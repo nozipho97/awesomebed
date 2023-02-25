9427622f2aec319a99ca1767f4fafc9f1533e7cb
nozipho97 <mayilanozipho@gmail.com>
2023-02-25 16:19:39 +0200

    last commit for the frontend

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const bedUrl = 'https://dashboard.render.com/web/srv-cft0iqh4reb6ks5rqn4g/deploys/dep-cft0irp4reb6ks5rr7sgL';
 
 export default createStore({
   state: {
+    users:null,
+    user:null,
+    products:null,
+    product: null,
+    showSpinner: null,
+    messege: null
+    
   },
   getters: {
   },
   mutations: {
+    setUsers(state, value) {
+      state.user=value
+    }
   },
   actions: {
+    async fetchUsers(context) {
+      const res = await axios.get(`${bedUrl}Users`);
+      const { result, err } = await res.data;
+      if (results) {
+        context.commit(`setUsers`,results)
+      } else {
+        context.commit('setMessege',err)
+      }
+    }
   },
   modules: {
   }
---
+++ src/views/AdminView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h2>Container</h2>
+    </div>
+    <div class="row">
+      
+    </div>
+  </div>
+</template>
